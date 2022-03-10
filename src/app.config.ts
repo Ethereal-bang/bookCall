38b6947b7fa5d35dbb5b6741bef3317e6709f894
Ethereal-bang <939847757@qq.com>
2022-03-10 09:15:42 +0800

    fix: "原发布书籍页面路径删除"

--- src/app.config.ts
+++ src/app.config.ts
@@ -5,8 +5,6 @@ export default {
     'pages/publish/publish',
     "pages/person/person",
     /*其他页面:*/
-    'pages/bookOut/bookOut',  // 主动换出
-    'pages/bookIn/bookIn',  // 希望换入
     "pages/bookList/bookList",  // 书籍列表
     "pages/bookGenreList/bookGenreList",  // 种类列表
     "pages/bookDetail/bookDetail" // 书籍详情
