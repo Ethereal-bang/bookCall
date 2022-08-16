c9409dfa6178414e2c84a56b2271e9b7eeefb42c
Ethereal-bang <939847757@qq.com>
2022-08-16 09:21:24 +0800

    fix: taro-axios replace axios

--- src/api/myAxios.ts
+++ src/api/myAxios.ts
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from "taro-axios";
 
 export const myAxios = axios.create({
   baseURL: "https://bookchang.com:8443/api/sql",
-  method: "Get",
+  method: "get",
 })
