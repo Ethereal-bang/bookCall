89f66bd014de3a238fa2feecda5d311882f5a59a
Ethereal-bang <939847757@qq.com>
2022-08-19 09:31:24 +0800

    fix: 消息

--- src/pages/news/news.vue
+++ src/pages/news/news.vue
@@ -1,18 +1,20 @@
 <template>
-  <!--小贴士-->
   <view>
-    小贴士：<br/>
-    -可以尝试从对方主页中看看有没有你喜欢的书，以便更好的达成交换目的~<br/>
-    -换书成功后即使下架书籍，沟通更高效~
-  </view>
+    <!--小贴士-->
+    <view>
+      <view>小贴士：</view>
+      <view>-可以尝试从对方主页中看看有没有你喜欢的书，以便更好的达成交换目的~</view>
+      <view>-换书成功后即使下架书籍，沟通更高效~</view>
+    </view>
 
-  <!--消息列表-->
-  <view>什么消息也没有~</view>
+    <!--消息列表-->
+    <view>什么消息也没有~</view>
+  </view>
 </template>
 
 <script>
 export default {
-  name: "news"
+  name: "News"
 }
 </script>
 
