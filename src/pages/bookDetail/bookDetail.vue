164b423de38d0bdc9c0db11e233a1cdf2ceb1f49
Ethereal-bang <939847757@qq.com>
2022-08-20 11:20:56 +0800

    update: 书籍详情

--- src/pages/bookDetail/bookDetail.vue
+++ src/pages/bookDetail/bookDetail.vue
@@ -1,78 +1,91 @@
 <template>
   <view class="bookDetail">
+    <!--用户信息栏-->
     <PersonalBar
       :user-name="bookData.username"
       :user-id="userData.id"
       :avatar-url="bookData.img"
     />
 
-<!--    <image-->
-<!--      :src="imgPaths[bookData.img]"-->
-<!--      class="bookimg"-->
-<!--      mode="aspectFit"-->
-<!--    />-->
-  <view class="bookmes">
-    <view class="bookname">{{ bookData.name }}</view>
+    <!--书籍封面-->
+    <!--    <image-->
+    <!--      :src="imgPaths[bookData.img]"-->
+    <!--      class="bookimg"-->
+    <!--      mode="aspectFit"-->
+    <!--    />-->
 
-    <view>
-      <AtTag class="booktag" size="small"
-        v-if="bookData.states === '可换'"
-        :active="true"
-      >
-        可换
-      </AtTag>
-      <AtTag
-        v-if="bookData.states === '求换'" size="small"
-        :active="true"
-                 class="booktag"
-      >
-        求换
-      </AtTag>
-
-      <AtTag size="small"
-                :active="true"
-                 class="booktag">{{ bookData.old }}成新</AtTag>
-      <AtTag size="small"
-          :active="true"
-                 class="booktag"  key="">{{ bookData.genre }}</AtTag>
-    </view>
-    <view>
-      <text>换书寄语：</text>
-      <text>{{ bookData.words }}</text>
-    </view>
-
-    <view>
+    <!--书籍信息-->
+    <view class="bookmes">
+      <view class="bookname">{{ bookData.name }}</view>
+      <!--Label-->
       <view>
-        <text>作者：</text>
+        <AtTag class="booktag" size="small"
+               v-if="bookData.states === '可换'"
+               :active="true"
+        >
+          可换
+        </AtTag>
+        <AtTag
+          v-if="bookData.states === '求换'" size="small"
+          :active="true"
+          class="booktag"
+        >
+          求换
+        </AtTag>
+
+        <AtTag size="small"
+               :active="true"
+               class="booktag"
+        >{{ bookData.old }}成新
+        </AtTag>
+        <AtTag size="small"
+               :active="true"
+               class="booktag" key=""
+        >{{ bookData.genre }}
+        </AtTag>
+      </view>
+
+      <!--作者-->
+      <view>
+        <text>作者</text>
         <text>{{ bookData.author }}</text>
       </view>
+
+      <!--出版社-->
       <view>
-        <text>ISBN：</text>
-        <text>暂无</text>
+        <text>出版社</text>
+        <text>{{ bookData.press }}</text>
       </view>
+
+      <!--出版社-->
+      <view>
+        <text>出版时间</text>
+        <text>{{ bookData.publishTime }}</text>
+      </view>
+
+      <!--简介-->
+      <view>
+        <text>图书简介</text>
+        <text>{{ bookData.description }}</text>
+      </view>
+
+      <!--寄语-->
+      <view>
+        <text>换书寄语：</text>
+        <text>{{ bookData.words }}</text>
+      </view>
+
     </view>
-</view>
+
     <button
-      v-if="!isOwn"
+      v-if="!userData.isOwn"
       @tap="handle"
       class="button"
     >
       找Ta换
     </button>
-    <view id="info" class="hide">
-      <view>请选择以下联系方式联系这位同学</view>
-      <view>更多的交流才能更好的完成换书~(点击即可复制)</view>
-      <view @tap="clip">
-        <text>微信:</text>
-        <text>XXXXXX</text>
-      </view>
-      <view>
-        <text>电话:</text>
-        <text>未留下该方式</text>
-      </view>
-    </view>
     <button
-      v-if="isOwn"
+      v-if="userData.isOwn"
       class="button"
     >
       下架
@@ -82,34 +95,31 @@
 
 <script>
 import PersonalBar from "../../components/personBar/PersonalBar";
-import { AtTag } from 'taro-ui-vue';
-import Taro from "@tarojs/taro";
+import {AtTag} from 'taro-ui-vue';
 import './bookDetail.scss';
-// const imgPaths = require("../../utils/base64");
 
 export default {
   name: "BookDetail",
   data() {
     return {
       key: "",  // 书籍编号
-      isOwn: false,   // 是否为本人
       bookData: {
         "id": undefined,
-        "username": "Loading",
         "name": "Loading",
         "img": "",
         "states": "Loading",
         "words": "",
         "genre": "",
         "old": undefined,
-        "contact": "",
-        "author": ""
+        "author": "",
+        press: "Loading", // 出版社
+        publishTime: "",  // 出版时间
+        description: "",  // 图书简介
       },
       userData: {
-        "id": undefined,
-        "username": "",
-        "words": "",
-        "contact": undefined,
+        id: undefined,
+        username: "Loading",
+        isOwn: false,   // 是否为本人
       }, // 书籍所有者信息
     }
   },
@@ -120,19 +130,10 @@ export default {
   onLoad: function (options) {
     console.log(options)
     this.key = options.key;
-    this.bookData = books[this.key - 1];
-    // 找到对应用户
-    for (const user of users) {
-      if (user.username === this.bookData.username) {
-        this.userData = user;
-        break;
-      }
-    }
-    console.log(this.bookData,this.userData)
-    // 写死，后面改！:
-    if (this.bookData.name === "编译原理") {
-      this.isOwn = true;
-    }
+    // 请求书籍信息;
+    // ...获取对应用户信息
+    console.log(this.bookData, this.userData)
+    // ...是否属于本人
   },
   methods: {
     handle() {
@@ -140,14 +141,6 @@ export default {
       info.removeAttribute("class");
       info.setAttribute("class", "show")
     },
-    clip() {  // 点击复制
-      Taro.setClipboardData({
-        data: "微信号_Test",
-        success: (res) => {
-          console.log("success")
-        }
-      })
-    }
   }
 }
 </script>
@@ -156,6 +149,7 @@ export default {
 .show {
   display: block;
 }
+
 .hide {
   display: none;
 }
